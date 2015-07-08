3805dda4cddac23e0e74a5f95920e3ef45299cc8
hperezrodal <hperezrodal@gmail.com>
2015-07-08 16:52:01 -0300

    Initial commit

--- nephele-backend/grails-app/conf/BuildConfig.groovy
+++ nephele-backend/grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,9 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.29'
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
-        test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
+		runtime 'postgresql:postgresql:8.4-702.jdbc3'
+		
+		test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
     }
 
     plugins {
@@ -62,6 +64,10 @@ grails.project.dependency.resolution = {
         compile ':cache:1.1.8'
         compile ":asset-pipeline:1.9.9"
 
+		//heroku
+		compile ':heroku:1.0.1'
+		compile ':cloud-support:1.0.11'
+		
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
--- nephele-backend/grails-app/conf/DataSource.groovy
+++ nephele-backend/grails-app/conf/DataSource.groovy
@@ -28,6 +28,21 @@ environments {
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
+	production {
+		dataSource {
+			dbCreate = "update"
+			driverClassName = "org.postgresql.Driver"
+			dialect = org.hibernate.dialect.PostgreSQLDialect
+	
+			uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+	
+			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+			username = uri.userInfo.split(":")[0]
+			password = uri.userInfo.split(":")[1]
+		}
+	}
+	
+	/*
     production {
         dataSource {
             dbCreate = "update"
@@ -53,5 +68,5 @@ environments {
                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
             }
         }
-    }
+    }*/
 }
